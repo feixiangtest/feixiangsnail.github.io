c718910bcc8af3bd8f76f845a3821c0d26a9d750
feixiangsnail <feixiangsnail@Hotmail.com>
2016-05-20 16:25:41 +0800

    second

---
+++ 音频播放器/js/playlist.js
@@ -0,0 +1,30 @@
+var res=[
+	{
+		"title": "倾国倾城",
+		"artist": "熊汝霖",
+		"album": "光荣与梦想",
+		"img":"img/1.jpg",
+		"mp3": "music/熊汝霖 - 倾国倾城.mp3"
+	},
+	{
+		"title": "幸运就是遇见你",
+		"artist": "周华健",
+		"album": "百年经典",
+		"img": "img/2.jpg",
+		"mp3": "music/周华健 - 花心.mp3"
+	},
+	{
+		"title": "我是明星",
+		"artist": "周华健",
+		"album": "奥运专辑",
+		"img": "img/3.jpg",
+		"mp3": "music/周华健 - 我是明星.mp3"
+	},
+    {
+		"title": "cool kids",
+		"artist": "echosmith",
+		"album": "billboard榜单",
+		"img": "img/4.jpg",
+		"mp3": "music/Echosmith - Cool Kids.mp3"
+	}
+]
