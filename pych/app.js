f0ec2ab4957e4216e3c4850b6695855c0f4bf453
feixiangsnail <feixiangsnail@hotmail.com>
2020-11-20 16:02:24 +0800

    finish

--- pych/app.js
+++ pych/app.js
@@ -1,6 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const Agent = require('socks5-https-client/lib/Agent');
+let request = require("request");
+let configData = require('./config').configData
 
+let msgId = ''
 const token = '1416043424:AAEsGDNnL5vBAJYs6836ldwoAYCLgFNNZTU';
 const bot = new TelegramBot(token, {
   polling: true,
@@ -13,15 +16,78 @@ const bot = new TelegramBot(token, {
 });
 
 // 匹配/hentai
-bot.onText(/\/hentai/, function onLoveText(msg) {
-  bot.sendMessage(msg.chat.id, 'Are you a hetai?');
+bot.onText(/\/cxgp/, function onLoveText(msg) {
+  bot.sendMessage(msg.chat.id, '正在帮您实时推送最近'+configData.withinTime+'小时内的相关信息');
+  msgId = msg.chat.id
+  console.log(msgId,'msgId')
+  runAll()
+setInterval(runAll,1000*5)
 });
 
 
-// 匹配/echo
-bot.onText(/\/echo (.+)/, (msg, match) => {
+// // 匹配/echo
+// bot.onText(/\/echo (.+)/, (msg, match) => {
 
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  bot.sendMessage(chatId, resp);
+//   const chatId = msg.chat.id;
+//   const resp = match[1];
+//   bot.sendMessage(chatId, resp);
+// });
+let keyName = []
+setInterval(()=>{
+    keyName=[]
+},1000*60*60*24)
+// runAll()
+// setInterval(runAll,1000*5)
+function runAll(){
+    console.log('开始查询最近'+configData.withinTime+'小时的数据')
+    configData.urls.map(item=>{
+        getData(item.url,item.userName)
+    })
+}
+function getData(url,userName){
+    
+    request(url, function (error, response, body) {
+  if (!error) {
+    // console.log(body);
+    let allData = JSON.parse(body)
+    // console.log(allData.rebalancing.rebalancing_histories)
+    allData.rebalancing.rebalancing_histories.map(item=>{
+        let onlyKey = item.id+'_'+item.updated_at
+        let passedTime = Date.now()-item.updated_at
+        if(keyName.indexOf(onlyKey)===-1&&passedTime<3600*1000*configData.withinTime){
+            keyName.push(onlyKey)
+            let msgContent = `用户:${userName||allData.rebalancing.id}在${dateFormat("YYYY-mm-dd HH:MM",new Date(item.updated_at))}所持有的：${item.stock_name+'，代码为'+item.stock_symbol}从${Number(item.prev_weight_adjusted)}%变为${item.target_weight}%,成交价为${item.price}`
+            
+            if(msgId){
+              bot.sendMessage(msgId, msgContent);
+            }
+            console.log(msgId,msgContent)
+        }
+        // resultData.push()
+    })
+    
+  }
+  
 });
+}
+function dateFormat(fmt, date) {
+    let ret;
+    const opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
+
--- pych/config.js
+++ pych/config.js
@@ -4,5 +4,5 @@ exports.configData= {
         {url:'https://xueqiu.com/cubes/rebalancing/show_origin.json?rb_id=85024246&cube_symbol=ZH188511',userName:'张三'},
         {url:'https://xueqiu.com/cubes/rebalancing/show_origin.json?rb_id=84935173&cube_symbol=ZH001784',userName:'小尘埃ZH001784'}
     ],
-    withinTime:12
+    withinTime:48
 }
